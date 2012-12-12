7ed04aa144788e078220387221ab40c4820d2a14
Roman Heinrich <roman.heinrich@gmail.com>
2012-12-13 01:25:19 +0200

    those methods seem to belong to action_handling, too...

--- lib/e/helpers/action_handling.rb
+++ lib/e/helpers/action_handling.rb
@@ -102,4 +102,70 @@ class E
     body.is_a?(Array) ? body.inject('') {|b,c| b << c.to_s} : body
   end
 
+  # same as `halt` except it uses for body the proc defined by `error` at class level
+  #
+  # @example
+  #    class App < E
+  #
+  #      # defining the proc to be executed on 404 errors
+  #      error 404 do |message|
+  #        render_view('layouts/404'){ message }
+  #      end
+  #
+  #      get :index do |id, status|
+  #        item = Model.fisrt id: id, status: status
+  #        unless item
+  #          # interrupt execution and send 404 error to browser.
+  #          error 404, 'Can not find item by given ID and Status'
+  #        end
+  #        # if no item found, code here will not be executed
+  #      end
+  #    end
+  def error status, body = nil
+    (handler = self.class.error?(status)) &&
+      (body = handler.last > 0 ? self.send(handler.first, body) : self.send(handler.first))
+    halt status.to_i, body
+  end
+
+  # Serving static files.
+  # Note that this blocks app while file readed/transmitted(on WEBrick and Thin, as minimum).
+  # To avoid app locking, setup your Nginx/Lighttpd server to set proper X-Sendfile header
+  # and use Rack::Sendfile middleware in your app.
+  #
+  # @param [String] path full path to file
+  # @param [Hash] opts
+  # @option opts [String] filename the name of file displayed in browser's save dialog
+  # @option opts [String] content_type custom content_type
+  # @option opts [String] last_modified
+  # @option opts [String] cache_control
+  # @option opts [Boolean] attachment if set to true, browser will prompt user to save file
+  def send_file path, opts = {}
+
+    file = ::Rack::File.new nil
+    file.path = path
+    (cache_control = opts[:cache_control]) && (file.cache_control = cache_control)
+    response = file.serving env
+
+    response[1][HEADER__CONTENT_DISPOSITION] = opts[:attachment] ?
+        'attachment; filename="%s"' % (opts[:filename] || ::File.basename(path)) :
+        'inline'
+
+    (content_type = opts[:content_type]) &&
+      (response[1][HEADER__CONTENT_TYPE] = content_type)
+
+    (last_modified = opts[:last_modified]) &&
+      (response[1][HEADER__LAST_MODIFIED] = last_modified)
+
+    halt response
+  end
+
+  # serve static files at dir path
+  def send_files dir
+    halt ::Rack::Directory.new(dir).call(env)
+  end
+
+  # same as `send_file` except it instruct browser to display save dialog
+  def attachment path, opts = {}
+    halt send_file path, opts.merge(:attachment => true)
+  end
 end
