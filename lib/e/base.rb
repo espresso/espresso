6022ac9d88aed49eabc61ceeff42e5f92306298d
Roman Heinrich <roman.heinrich@gmail.com>
2012-12-13 01:40:09 +0200

    moved stuff form e_app.rb to base.rb (static methods for E)

--- lib/e/base.rb
+++ lib/e/base.rb
@@ -1,5 +1,4 @@
 class E
-
   def user
     env[ENV__REMOTE_USER]
   end
@@ -16,5 +15,40 @@ class E
   # Sugar for redirect (example:  redirect back)
   def back
     request.referer
+  end
+end
+
+
+class << E
+
+  def mount *roots, &setup
+    return app if app
+    locked? && raise(SecurityError, 'App was previously locked, so you can not remount it or change any setup.')
+    ::EApp.new.mount self, *roots, &setup
+  end
+  alias mount!  mount
+  alias to_app  mount
+  alias to_app! mount
+
+  def call env
+    mount.call env
+  end
+
+  def run *args
+    mount.run *args
+  end
+
+  # @api semi-public
+  def app= app
+    return if locked?
+    @app = app
+    # overriding @base_url by prepending app's base URL.
+    # IMPORTANT: @base_url is a var set by Appetite,
+    # so make sure when this name is changed in Appetite it is also changed here
+    @base_url = @app.base_url + base_url
+  end
+
+  def app
+    @app
   end
 end
--- lib/e/e_app.rb
+++ lib/e/e_app.rb
@@ -421,37 +421,3 @@ class EApp
   end
 
 end
-
-class << E
-
-  def mount *roots, &setup
-    return app if app
-    locked? && raise(SecurityError, 'App was previously locked, so you can not remount it or change any setup.')
-    ::EApp.new.mount self, *roots, &setup
-  end
-  alias mount!  mount
-  alias to_app  mount
-  alias to_app! mount
-
-  def call env
-    mount.call env
-  end
-
-  def run *args
-    mount.run *args
-  end
-
-  # @api semi-public
-  def app= app
-    return if locked?
-    @app = app
-    # overriding @base_url by prepending app's base URL.
-    # IMPORTANT: @base_url is a var set by Appetite,
-    # so make sure when this name is changed in Appetite it is also changed here
-    @base_url = @app.base_url + base_url
-  end
-
-  def app
-    @app
-  end
-end
