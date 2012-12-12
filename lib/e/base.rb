03adb08c680eb8e805049aadd476051075ebba32
Roman Heinrich <roman.heinrich@gmail.com>
2012-12-13 00:21:57 +0200

    http_stati to extra file

--- lib/e/base.rb
+++ lib/e/base.rb
@@ -290,36 +290,6 @@ class E
     request.referer
   end
 
-  # whether or not the status is set to 1xx
-  def informational?
-    status.between? 100, 199
-  end
-
-  # whether or not the status is set to 2xx
-  def success?
-    status.between? 200, 299
-  end
-
-  # whether or not the status is set to 3xx
-  def redirect?
-    status.between? 300, 399
-  end
-
-  # whether or not the status is set to 4xx
-  def client_error?
-    status.between? 400, 499
-  end
-
-  # whether or not the status is set to 5xx
-  def server_error?
-    status.between? 500, 599
-  end
-
-  # whether or not the status is set to 404
-  def not_found?
-    status == 404
-  end
-
   # Generates a Time object from the given value.
   # Used by #expires and #last_modified.
   def time_for(value)
