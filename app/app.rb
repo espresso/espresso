3b96ba69ad9d8ac2183282d9731cb3a4c8514d68
slivu <slivuz@gmail.com>
2013-02-05 13:40:26 +0200

    generator - controllers - 1

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../app/boot', __FILE__)
 
 options = {}
-(server = Cfg.server) && (options[:server] = server)
-(port   = Cfg.port  ) && (options[:port  ] = port  )
+(server = Cfg[:server]) && (options[:server] = server)
+(port   = Cfg[:port]  ) && (options[:port  ] = port  )
+
 App.run options
