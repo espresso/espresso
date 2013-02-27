e9a1b0b9dc7e9d665f41ab6e02c76033488eeeb8
slivu <slivuz@gmail.com>
2013-02-27 15:09:21 +0200

    link_to helper - more tests

--- test/e-more/test__link_to.rb
+++ test/e-more/test__link_to.rb
@@ -2,8 +2,12 @@ module EMoreTest__LinkTo
   class App < E
     map '/'
     format_for :foo, '.bar'
+    format '.html'
 
     def foo
+    end
+
+    def post_bar
     end
   end
   App.mount
@@ -19,7 +23,7 @@ module EMoreTest__LinkTo
   
   Spec.new self do
     app = App.new
-    
+
     expect( app.link_to(:foo) ) =~ %r[href="/foo">/foo]
     expect( app.link_to(:foo, 'some-label') ) =~ %r[href="/foo">some-label]
     
@@ -35,9 +39,15 @@ module EMoreTest__LinkTo
     expect( app.link_to(:foo, target: '_blank') { 'some-label' } ) =~
       %r[href="/foo" target="_blank">some-label]
 
+    false?( app.link_to('foo.html') ) =~ %r[href="/foo.html"]
     expect( app.link_to('foo.bar') ) =~ %r[href="/foo.bar"]
 
     expect( app.link_to App2[:foo] ) =~ %r[href="/app2/foo"]
     expect( app.link_to App2['foo.bar'] ) =~ %r[href="/app2/foo.bar"]
+
+    expect( app.link_to(:bar) ) =~ %r[href="/bar"]
+    expect( app.link_to('bar.html') ) =~ %r[href="/bar.html"]
+    expect( app.link_to(:baz) ) =~ %r[href="baz"]
+    expect( app.link_to('baz.html') ) =~ %r[href="baz.html"]
   end
 end
