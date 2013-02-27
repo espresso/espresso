9ddf777c92239f3590b3b4212d4ec8578ae23fce
slivu <slivuz@gmail.com>
2013-02-27 23:41:20 +0200

    use CommonLogger and ShowExceptions on dev env

--- app/base/base/boot.rb
+++ app/base/base/boot.rb
@@ -11,15 +11,22 @@ Bundler.require(Cfg.env)
 
 require Cfg.base_path('database.rb')
 
-App = EspressoApp.new(:automount)
-App.controllers_setup do
-  view_path 'base/views'
-  engine(Cfg[:engine]) if Cfg[:engine]
-  format(Cfg[:format]) if Cfg[:format]
-end
+App = EspressoApp.new :automount do
+  
+  controllers_setup do
+    view_path 'base/views'
+    engine(Cfg[:engine]) if Cfg[:engine]
+    format(Cfg[:format]) if Cfg[:format]
+  end
 
-App.assets_url 'assets'
-App.assets.prepend_path Cfg.assets_path
+  assets_url 'assets'
+  assets.prepend_path Cfg.assets_path
+
+  if Cfg.dev?
+    use Rack::CommonLogger
+    use Rack::ShowExceptions
+  end
+end
 
 [Cfg.helpers_path, Cfg.models_path].each do |path|
   Dir[path + '**/*.rb'].each {|f| require f}
