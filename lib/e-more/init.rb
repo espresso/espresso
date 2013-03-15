baf7565868159278aedc2bd2003bbc069c576cd7
Vince Salinas <kojul@kojul.com>
2013-03-15 18:20:15 +0000

    More cleanup and register Slim

--- lib/e-more/init.rb
+++ lib/e-more/init.rb
@@ -2,6 +2,10 @@ module EspressoConstants
 
   VIEW__ENGINE_BY_EXT, VIEW__ENGINE_BY_SYM = {}, {}
   
+  if Object.const_defined?(:Slim)
+    Tilt.register 'slim', Slim::Template
+  end
+
   if Object.const_defined?(:Rabl)
     Rabl.register!
   end
@@ -15,15 +19,6 @@ module EspressoConstants
       VIEW__ENGINE_BY_SYM[engine_name.to_sym] = engine
     end
   end
-  # Rabl adapter not shipped with Tilt,
-  # so adding Rabl to map to be sure adhoc methods are defined at loadtime
-  VIEW__ENGINE_BY_EXT['.rabl'] = nil unless VIEW__ENGINE_BY_EXT.has_key?('.rabl')
-  VIEW__ENGINE_BY_SYM[:Rabl]   = nil unless VIEW__ENGINE_BY_SYM.has_key?(:Rabl)
-
-  # Slim adapter not shipped with Tilt,
-  # so adding Slim to map to be sure adhoc methods are defined at loadtime
-  VIEW__ENGINE_BY_EXT['.slim'] = nil unless VIEW__ENGINE_BY_EXT.has_key?('.slim')
-  VIEW__ENGINE_BY_SYM[:Slim]   = nil unless VIEW__ENGINE_BY_SYM.has_key?(:Slim)
 
   VIEW__EXT_BY_ENGINE = Tilt.mappings.sort { |a, b| b.first.size <=> a.first.size }.
     inject({}) { |m, i| i.last.each { |e| m.update e => ('.' + i.first).freeze }; m }
